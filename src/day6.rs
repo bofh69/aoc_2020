242085d53362acf6326ea86b492c95d33ca2111f
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2020-12-06 10:07:44 +0100

    Optimized version

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,8 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::HashSet;
+// use std::collections::HashSet;
 // use ::regex::*;
 
-type Data = Vec<HashSet<char>>;
+type Data = Vec<u32>;
 
 #[aoc_generator(day6)]
 pub fn input_generator(input: &str) -> Vec<Data> {
@@ -11,36 +11,32 @@ pub fn input_generator(input: &str) -> Vec<Data> {
         .map(|group| {
             group
                 .lines()
-                .map(|person| person.chars().filter(|&c| c != '\n').collect())
+                .map(|person| person.chars().map(|c| 1 << (c as u32 - 'a' as u32)).sum())
                 .collect()
         })
         .collect()
 }
 
 #[aoc(day6, part1)]
-pub fn solve_part1(data: &[Data]) -> usize {
+pub fn solve_part1(data: &[Data]) -> u32 {
     data.iter()
         .map(|group| {
-            let mut union = HashSet::new();
-            group.iter().for_each(|person| {
-                person.iter().for_each(|answer| {
-                    union.insert(answer);
-                })
-            });
-            union.len()
+            group
+                .iter()
+                .fold(0, |acc, person| acc | person)
+                .count_ones()
         })
         .sum()
 }
 
 #[aoc(day6, part2)]
-pub fn solve_part2(data: &[Data]) -> usize {
+pub fn solve_part2(data: &[Data]) -> u32 {
     data.iter()
         .map(|group| {
-            let acc = group[0].clone();
-            let g = group.iter().fold(acc, |acc, person| {
-                acc.intersection(person).copied().collect()
-            });
-            g.len()
+            group
+                .iter()
+                .fold(!0, |acc, person| acc & person)
+                .count_ones()
         })
         .sum()
 }
