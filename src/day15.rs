41e0b67cca6da9be5eda8c8ee3897ac755f09565
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2020-12-15 19:49:04 +0100

    Optimize part 2 with vector

--- src/day15.rs
+++ src/day15.rs
@@ -33,20 +33,20 @@ pub fn solve_part1(data: &[Data]) -> usize {
 #[aoc(day15, part2)]
 pub fn solve_part2(data: &[Data]) -> usize {
     const GOAL: usize = 30_000_000;
-    let mut previous = HashMap::new();
+    let mut previous = vec![GOAL; GOAL];
     for (i, number) in data.iter().enumerate() {
-        previous.insert(*number, i);
+        previous[*number] = i;
     }
     let mut last: usize = data[data.len() - 1];
-    let mut before: Option<usize> = None;
+    let mut before: usize = GOAL;
     for turn in data.len()..GOAL {
-        if let Some(prev_turn) = before {
-            last = turn - 1 - prev_turn;
+        if before != GOAL {
+            last = turn - 1 - before;
         } else {
             last = 0;
         }
-        before = previous.get(&last).map(|&n| n);
-        previous.insert(last, turn);
+        before = previous[last];
+        previous[last] = turn;
     }
     last
 }
